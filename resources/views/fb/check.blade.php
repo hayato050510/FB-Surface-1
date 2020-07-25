8bd46cedf199c546666251f9bfe02398955b989d
EC2 Default User <ec2-user@ip-172-31-25-228.us-east-2.compute.internal>
2020-07-25 00:35:55 +0000

    Mm

---
+++ resources/views/fb/check.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>公式ページへ移動中</title>
+        <link href="{{ secure_asset('css/check.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <br>
+        <img src="{{ asset('image/fwtch.png') }}"></img>
+        <div class="button">
+                <a href="https://apps.apple.com/jp/app/%E3%81%B5%E3%82%8F%E3%81%A3%E3%81%A1/id1106217401" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
+        </div>
+        <p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>
+    </body>
+</html>
